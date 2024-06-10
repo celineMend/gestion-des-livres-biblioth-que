1613d700859ad7433cb55118a3aa87be0f2a908c
celineMend <celine.mendy@unchk.edu.sn>
2024-06-10 12:22:42 +0100

    réglage de la base de donnée pour le déploiement

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,50 @@
 <?php
 
-namespace Database\Seeders;
+// database/seeders/DatabaseSeeder.php
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Livre;
+use App\Models\Rayon;
+use App\Models\Categorie;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        // Créer quelques catégories
+        $categorie1 = Categorie::create(['libelle' => 'Catégorie 1', 'description' => 'Description 1']);
+        $categorie2 = Categorie::create(['libelle' => 'Catégorie 2', 'description' => 'Description 2']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Créer quelques rayons
+        $rayon1 = Rayon::create(['libelle' => 'Rayon 1', 'partie' => 'première moitié']);
+        $rayon2 = Rayon::create(['libelle' => 'Rayon 2', 'partie' => 'première moitié']);
+
+
+
+        // Créer quelques livres
+        Livre::create([
+            'titre' => 'Livre 1',
+            'image' => 'image1.jpg',
+            'date_de_publication' => '2023-01-01',
+            'nombre_de_pages' => '100',
+            'auteur' => 'Auteur 1',
+            'isbn' => '1234567890',
+            'editeur' => 'Editeur 1',
+            'categorie_id' => $categorie1->id,
+            'rayon_id' => $rayon1->id,
+        ]);
+
+        Livre::create([
+            'titre' => 'Livre 2',
+            'image' => 'image2.jpg',
+            'date_de_publication' => '2023-02-01',
+            'nombre_de_pages' => '200',
+            'auteur' => 'Auteur 2',
+            'isbn' => '0987654321',
+            'editeur' => 'Editeur 2',
+            'categorie_id' => $categorie2->id,
+            'rayon_id' => $rayon2->id,
         ]);
     }
 }
+
